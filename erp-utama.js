9644717059bb488ae5ddddd00f03de3d6c9d5db3
skuy99 <mangekujang2@gmail.com>
2019-10-11 12:50:45 +0700

    Update erp-utama.js

--- erp-utama.js
+++ erp-utama.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2019/08/the-benefits-of-erp-for-small-business.html"}
 if(x=="2"){xxx="/2019/08/what-does-erp-software-mean-for-your.html"}
 if(x=="3"){xxx="/2019/08/the-biggest-erp-software-companies.html"}
 if(x=="4"){xxx="/2019/08/how-to-choose-erp-software-for-midsize.html"}
-if(x=="5"){xxx="/2019/08/the-best-enterprise-erp-software.html"}a_to_ve[i].href="https://countmoney7.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2019/08/the-best-enterprise-erp-software.html"}a_to_ve[i].href="https://erpsoftware.evansteele.me"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
